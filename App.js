bd52646f69a857457ce51bcfa59ed66128b9c08b
bindujars <116160632+bindujars@users.noreply.github.com>
2022-10-30 08:59:53 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,51 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import Header from './app/Header';
 
 function App() {
   return (
-   
+       
+    <div class="a">
+      
+        <div class="z"><img src="shine.jpg" class="logo" alt="shineapp"/>
+            <div class="box"><i class="fa-solid fa-magnifying-glass"></i><input type="text" placeholder="Job title,Skills"/></div></div>
+            <br/> 
+    <div class="b">
+        <ul>
+        <li><a href="https://www.shine.com/">home</a></li>
+        <li><a href="https://www.shine.com/">jobs</a></li>
+        <li><a href="https://resume.shine.com/">job seeking assistance</a></li>
+        <li><a href="https://learning.shine.com/">courses</a></li>
+        <li><a href="https://learning.shine.com/user-intent">career guidance</a></li>
+        <li><a href="https://learning.shine.com/talenteconomy/">blog</a></li>
+            <li style="font-size: 10px; " ><a href="https://www.shine.com/registration/parser/"><i class="fa-light fa-circle-question"></i>Download App</a></li><li style="font-size: 10px; " ><a href=""><i class="fa-light fa-circle-question"></i>Help</a></li> <li style="font-size: 10px; " ><a href="https://recruiter.shine.com/">Login</a></li>
+        </ul></div>
+    <div class="y">
+        <h3>Amazon is Hiring</h3><h2>Quality Specialist-LMAQ</h2><h4>Location Bangalore</h4> <a href="https://amazonvirtualhiring.hirepro.in/registration/incta/ju0f4/apply/?j=56958&e=12850"><button type="button"><span></span>APPLY NOW</button></a> <h5>APRIL 13-20</h5>         
+       </div><br/>
+        <div class="t"><h1>ARE YOU SEARCHING FOR A JOB</h1>
+        </div>
+        <br/>
+        <div class="j"> 
+            <div1><a href="https://www.infosys.com/"><img src="infosys.jpg" alt="infosys" width="260" height="200"/></a>  </div1>
 
-
-    <div >
-    <Header/>     
-    </div>
-  );}
-  
-
+            <div2><a href="https://careers.adityabirla.com/"><img src="adi.jpg" alt="birla" width="260" height="200"/></a> </div2>
+                        <div3><a href="https://www.chase.com/"><img src= "chase.jpg" alt="chase" width="260" height="200"/></a> </div3>
+            <div4><a href="https://www.genpact.com/"><img src="genpact.jpg" alt="birla" width="260" height="200"/></a> </div4>
+            </div>
+        <div class="o"><h1>Be   An   Early   Applicant</h1></div><br/>
+        <div class="k"><div5><h1>DevOps Developer</h1><h3>Linux,AWS,Ruby,Python</h3><h3>Location-Chennai<br/>1-3Yrs Experience</h3></div5>
+            <div6><h1> Network Operations Analyst</h1><br/><h3>Location - Pune<br/>0-3Yrs Experience</h3></div6>
+        <div7><h1>Senior Software Infrastructure Engineer-1</h1><h3>Location-Trivandrum<br/>3-4Yrs Experience</h3></div7></div>
+        <div class="e">
+        <div8><ul>jobs<ul>
+        <li><a href="https://www.shine.com/">home</a></li>
+        <li><a href="https://www.shine.com/">jobs</a></li>
+        <li><a href="https://resume.shine.com/">job seeking assistance</a></li>
+        <li><a href="https://learning.shine.com/">courses</a></li>
+        <li><a href="https://learning.shine.com/user-intent">career guidance</a></li>
+            <li><a href="https://learning.shine.com/talenteconomy/">blog</a></li></ul></ul></div8></div>
+       
+        
+            </div>
+          )};
 export default App;
